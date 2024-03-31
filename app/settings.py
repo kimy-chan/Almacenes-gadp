617d38cd5333d75536c3e578104264a8fe47cb6c
juan carlos chocllu <kimysheeran@gmail.com>
2024-03-31 17:05:04 -0400

    conexion a la base de datos ´postgres

--- app/settings.py
+++ app/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alamcenes-gadp',
+        'USER': 'postgres',
+        'PASSWORD': 'kanna',
+        'HOST': 'localhost', # o la dirección del servidor donde se encuentra tu base de datos
+        'PORT': '5432'
     }
 }
 
