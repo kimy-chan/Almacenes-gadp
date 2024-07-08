5228f5b61cf645b3f1c24fe1be941845f0fbf178
juan carlos chocllu <kimysheeran@gmail.com>
2024-07-07 22:18:10 -0400

    estilos

--- almacenes/materiales/urls.py
+++ almacenes/materiales/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import crear_categoria, crear_material, listado_material,informacion_material, softDelete, editar_material, inprimir
 urlpatterns = [
     path('crear_categoria', crear_categoria, name='crear_categoria'),
-      path('crear_producto', crear_material, name='crear_producto'), 
+      path('crear_material', crear_material, name='crear_material'), 
 
     path('<int:id_categoria>', listado_material, name='categorias_por_id'),  #ruta para listar los prodcutos por categoria  
     path('informacion/<int:id_material>',informacion_material, name='informacion_material' ),
